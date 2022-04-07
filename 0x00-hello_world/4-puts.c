c20026aae02ba8732641e9ba0c7ea3e35aa462ef
Michaelyilma9 <michaelyilma85@gmail.com>
2022-04-07 03:43:37 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main Entry point
+  * 
+  *
+  * return always 0 (sucess)
+  */
+int main(void)
+{
+	puts("programming is like building multilingual puzzle \n");
+	returns (0);
+}
