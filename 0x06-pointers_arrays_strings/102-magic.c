e7b2d2c1fdca744c6595c39a60f3d0cc7df1184b
Michaelyilma9 <michaelyilma85@gmail.com>
2022-04-21 13:51:48 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main(void)
 {
 int n;
@@ -8,8 +7,7 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-
- /*
+  /*
   * write your line of code here...
   * Remember:
   * - you are not allowed to use a
