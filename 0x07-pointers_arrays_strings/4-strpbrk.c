ca57ef808819bbfc1a18f6341f60d8ba4c380df7
Michaelyilma9 <michaelyilma85@gmail.com>
2022-04-25 10:37:31 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: pointer to string to search through
  * @accept: array of bytes to search for
- * Return: pointer to the byte in s that matches one of the bytes in accep 
+ * Return: pointer to the byte in s that matches one of the bytes in accept 
  * or return NULL if no bytes are found.
  */
 
