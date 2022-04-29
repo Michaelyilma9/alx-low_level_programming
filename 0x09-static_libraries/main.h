fe6f119291c40091abc1b129ef526ff56f0ea106
Michaelyilma9 <michaelyilma85@gmail.com>
2022-04-29 10:25:06 -0700

    task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -7,7 +7,8 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s);char *_strcpy(char *dest, char *src);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -19,5 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int string_length(char *pointer);
 #endif
