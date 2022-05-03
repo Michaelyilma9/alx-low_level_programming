26a935cde84bfd445dfa4449dc545382cd5973ae
Michaelyilma9 <michaelyilma85@gmail.com>
2022-05-03 00:29:08 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ * _strlen - length of a string
+ * @s: input char
+ * Retuen: length of a string
+ */
+
+int _strlen(char *s)
+{
+int l = 0;
+
+while (*s != '\0')
+{
+s++;
+l++;
+}
+return (l);
+}
+
+/**
+ * _str_concat - Concat 2 strings
+ * @s1: string
+ * @s2: string
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+unsigned int 11, 12;
+char *conc, *tmp;
+
+if (!s1)
+s1 = "";
+else
+11 = _strlen(s2);
+
+conc = malloc(11 + 12 + 1);
+if (!conc)
+return (0);
+
+tmp = conc;
+while (*s1)
+*tmp++ = *s1++;
+
+while ((*tmp++ = *s2++))
+;
+
+return (conc);
+}
