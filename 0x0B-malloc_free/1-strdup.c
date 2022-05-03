f656a5ea7afdd21130701dcb8430e3fdde47b6e4
Michaelyilma9 <michaelyilma85@gmail.com>
2022-05-03 00:15:49 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,47 +1,32 @@
-#include "holberton.h"
 #include <stdlib.h>
-#include <stddef.h>
 
 /**
- * *_strdup - returns pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
- *
- * @str: string.
- * Return: NULL if str = NULL or insufficient memory, if success return
- * pointer to duplicated string.
+ * _strdup - copy
+ * @str: pointer to string
+ * Return: pointer
  */
+
 char *_strdup(char *str)
 {
-int str_size, i;
-char *copy;
+char *s
+int i = 0, j;
 
-if (str == NULL)
+if (!str)
 return (NULL);
-/* allocating memory for the copy */
-str_size = string_length(str);
-copy = (char *)malloc(sizeof(char) * str_size + 1);
-if (copy == NULL)
-return (NULL);
-/* copy string */
-i = 0;
-while (str[i] != '\0')
-{											copy[i] = str[i];
+
+while (*(str + i))
 i++;
-}
-copy[i] = '\0';										return (copy);
-}
-/**
- * string_length - finds the length of a string.
- * Return: length of c.
- * @pointer: pointer.
-*/
-int string_length(char *pointer)
-{
-int c = 0;
+i++
+s = malloc(sizeof(char) * i);
 
-while (*(pointer + c) != '\0')
+if (s == NULL)
 {
-c++;
+return (NULL);
 }
-return (c);
+
+for (j = 0; j <= i; j++)
+{
+s[j] = str[j];
+}
+return (s);
 }
