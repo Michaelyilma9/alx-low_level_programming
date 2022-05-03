26820cf1156e6323ae7f1708d63e862ed3a1c15a
Michaelyilma9 <michaelyilma85@gmail.com>
2022-05-03 10:58:23 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,47 +1,51 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
- * * argstostr - a function that concatenates all the arguments of your program
- * *@ac: count of args passed to the function
- * *@av:array of arguments
- * *
- * *Return: pointer to the new string
- * */
-
+ * *argstostr - concatenates all the arguments of program
+ * @ac: argument counter
+ * @av; argument vector
+ * Return: NULL if ac == 0 or av == NULL else
+ * pointer to a string of NULL if it fails
+ */
 char *argstostr(int ac, char **av)
 {
-char *new_string = NULL;
-int k = 0, i = ac, j, sum = 0, temp = 0;
+int i, j, k, length;
+char *str;
 
+length = 0;
+k = 0;
 if (ac == 0 || av == NULL)
 return (NULL);
 
-while (ac--)
-sum += (len(av[ac]) + 1);								new_string = (char *) malloc(sum + 1);
-if (new_string != NULL)									while (k < i)										{											for (j = 0; av[k][j] != '\0'; j++)							new_string[j + temp] = av[k][j];
-new_string[temp + j] = '\n';
-temp += (j + 1);									k++;											}
-new_string[temp] = '\0';
-}
-else
+i = 0;
+while (i < ac)
 {
+j = 0;
+while (av[i][j])
+{
+length++;
+j++;
+}
+length++;
+i++;
+}
+str = malloc((sizeof(char) + length) + 1);
+if (str == NULL)
 return (NULL);
-}
-return (new_string);
-}
-
-/**
- *len - returns length of str
- *@str: string counted
- *Return: returns the length
- */
-int len(char *str)
+i = 0;
+while (i < ac)
 {
-int len = 0;
-
-if (str != NULL)
+j = 0;
+while (av[i][j])
 {
-while (str[len])
-len++;
+str[k] = av[i][j];
+j++;
+k++;
 }
-return (len);
+str[k] = '\n';
+k++;
+i++;
+}
+str[k] = '\0';
+return (str);
 }
