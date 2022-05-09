fb9fa9a717ae2dedd12ac9181bd93355f1dcca99
Michaelyilma9 <michaelyilma85@gmail.com>
2022-05-09 10:33:35 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,75 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * new_dog - creates a new struct of type dog
- * @name: struct parameter name
- * @age: struct parameter age
- * @owner: struct parameter owner
- * Return: returns pointer to buffer of datatype dog
+ * new_dog - makes a new dog, memory and all
+ * @name: name to create mem and assign to for dog
+ * @age: age to assign to for dog
+ * @owner: owner to create mem and assign to for dog
+ *
+ * Return: pointer to new dog_t (truct dog)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int nlen, olen, i;
-dog_t *doggy;
+dog_t *new_d;
 
-nlen = olen = 0;
-while (name[nlen++])
+new_d = malloc(sizeof(dog_t));
+if (new_d == NULL)
+return (NULL);
+if (name == NULL)
+new_d-<name = NULL;
+else
+{
+new_d->name = malloc(strlen(name) + 1);
+if (new_d->name == NULL)
+{
+free(new_d);
+return (NULL);
+}
+new_d->name = _strcpy(new_d->name, name);
+}
+if (owner == NULL)
+new_d->owner = NULL;
+else
+{
+new_d->owner = malloc(_strlen(owner) + 1);
+if (new_d->owner == NULL)
+{
+free(new_d->name);
+free(new_d);
+return (NULL);
+}
+new_d->owner = _strcpy(new_d->owner, owner);
+}
+new_d->age = age;
+return (new_d);
+}
+/**
+ * _strlen - gets len of str
+ * @s: string to get length of
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+for (i = 0; s[i]; i++)
 ;
-while (owner[olen++])
-;
-doggy = malloc(sizeof(dog_t));
-if (doggy == NULL)
-return (NULL);
+return (i);
+}
+/**
+ * _strcpy - copies string from y to x
+ * @x: pointer to destination of string
+ * @y: pointer to source string to copy from
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *x, char *y)
+{
+char *a = x;
 
-doggy->name = malloc(nlen *sizeof(doggy->name));
-if (doggy == NULL)
-return (NULL);
-for (i = 0; i < nlen; i++)
-doggy->name[i] = name[i];
-
-doggy->age = age;
-
-doggy->owner = malloc(olen *sizeof(doggy->owner));
-if (doggy == NULL)
-return (NULL);
-for (i = 0; i < olen; i++)
-doggy->owner[i] = owner[i];
-return (doggy);
+while (*y)
+*a++ = *y++;
+*a = '\0';
+return (x);
 }
