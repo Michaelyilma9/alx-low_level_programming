8a63caec8de6ca97d21a60d9e8b2526a17535099
Michaelyilma9 <michaelyilma85@gmail.com>
2022-05-12 10:54:00 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,29 @@
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
-
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-/**
- * struct print - print type with corresponding print function
- * @t: print type
- * @f: print function
- */
-typedef struct print
-{
-char *t
-void (*f)(va_list);
-} print_t;
+#include <stdio.h>
 
-int _putchar(char);
+/**
+ * struct fmt - Struct format
+ * @f: type format
+ * @p: print function
+ */
+typedef struct fmt
+{
+char *f;
+void (*p)(va_list s);
+} my_fmt;
+
+
+/*prototypes*/
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _strlen_recursion(char *s);
+void print_c(va_list s);
+void print_i(va_list s);
+void print_f(va_list s);
+void print_s(va_list s);
 
-#endif /* _VARIADIC_FUNCTIONS_H_ */
+#endif
